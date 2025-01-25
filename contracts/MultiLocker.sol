9f3785c22ea241ce5caa82a8d4097e928aeab793
Sagleft <sagleft@inbox.ru>
2025-01-25 16:55:58 +0300

    remove safe transfer - infinite gas

--- contracts/MultiLocker.sol
+++ contracts/MultiLocker.sol
@@ -2,11 +2,12 @@
 
 pragma solidity ^0.8.20;
 
-import "./SafeERC20.sol";
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
 
 contract MultiLocker {
-    using SafeERC20 for IERC20;
-
     address public owner;
     mapping(address => uint256) public lockedBalances;   // token address -> amount
     mapping(address => uint256) public lockedTimestamps; // token address -> unlockTimestamp
@@ -25,31 +26,33 @@ contract MultiLocker {
         owner = msg.sender; 
     }
 
-		// Lock Tokens for the specified duration
+    // Lock Tokens for the specified duration
     function lockTokens(address _token, uint256 _amount, uint256 _unlockTimestamp) external onlyOwner {
         require(_amount > 0, "Amount is zero");
         require(_unlockTimestamp > block.timestamp, "Invalid lock duration");
 
         // transfer tokens to contract
-        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+        bool success = IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        require(success, "Token transfer failed");
 
         // increment locked amount
-        lockedBalances[_token] = lockedBalances[_token] + _amount;
+        lockedBalances[_token] += _amount;
         if (_unlockTimestamp > lockedTimestamps[_token]) {
-          lockedTimestamps[_token] = _unlockTimestamp;
+            lockedTimestamps[_token] = _unlockTimestamp;
         }
 
         emit TokensLocked(msg.sender, _token, _amount, _unlockTimestamp);
     }
 
-		// Unlock Tokens after the specified duration
+    // Unlock Tokens after the specified duration
     function unlockTokens(address _token) external onlyOwner {
         require(lockedBalances[_token] > 0, "No tokens are locked for this token");
         require(block.timestamp >= lockedTimestamps[_token], "Tokens cannot be unlocked yet");
 
         // transfer tokens
         uint256 amount = lockedBalances[_token];
-        IERC20(_token).safeTransfer(owner, amount);
+        bool success = IERC20(_token).transfer(owner, amount);
+        require(success, "Token transfer failed");
 
         // reset lock
         lockedTimestamps[_token] = 0;
@@ -58,21 +61,20 @@ contract MultiLocker {
         emit TokensUnlocked(msg.sender, _token, amount);
     }
 
-		// Change owner of contract & receiver of token(s)
+    // Change owner of contract & receiver of token(s)
     function changeDestination(address _newDestination) external onlyOwner {
         require(_newDestination != address(0), "Invalid destination address");
         require(_newDestination != owner, "New destination must be different from current owner");
 
         // change contract owner
-        owner = _newDestination;
-
         emit DestinationChanged(owner, _newDestination);
+        owner = _newDestination;
     }
 
-		// Extend the lock duration (unix timestamp)
+    // Extend the lock duration (unix timestamp)
     function extendDuration(address _token, uint256 _newTimestamp) external onlyOwner {
         require(_newTimestamp > block.timestamp, "New lock duration must be greater than previous duration");
-        require(_newTimestamp > lockedTimestamps[_token], "New timestamp shold be greater than old timestamp");
+        require(_newTimestamp > lockedTimestamps[_token], "New timestamp should be greater than old timestamp");
 
         // change locker duration
         lockedTimestamps[_token] = _newTimestamp;
